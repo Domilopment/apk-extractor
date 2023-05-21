eae42c98a32a1dd69542e46bc9acae77cdfb9245
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2023-05-21 17:33:45 +0200

    Update ProgressDialogFragment to DataBinding

--- app/src/main/java/domilopment/apkextractor/ui/ProgressDialogFragment.kt
+++ app/src/main/java/domilopment/apkextractor/ui/ProgressDialogFragment.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textview.MaterialTextView
 import domilopment.apkextractor.ui.viewModels.ProgressDialogViewModel
 import domilopment.apkextractor.R
 import domilopment.apkextractor.databinding.ProgressDialogBinding
@@ -22,10 +21,6 @@ class ProgressDialogFragment : DialogFragment() {
     private var _binding: ProgressDialogBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var progressBar: ProgressBar
-    private lateinit var textPercentages: MaterialTextView
-    private lateinit var textValue: MaterialTextView
-    private lateinit var currentProcess: MaterialTextView
     private val model by activityViewModels<ProgressDialogViewModel>()
 
     override fun onCreateView(
@@ -40,11 +35,6 @@ class ProgressDialogFragment : DialogFragment() {
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 model.progressDialogState.collect { uiState ->
-                    currentProcess.text = uiState.process ?: ""
-                    progressBar.progress = uiState.progress
-                    progressBar.max = uiState.tasks
-                    textPercentages.text = getPercentageString()
-                    textValue.text = getValueString()
                     if (!uiState.shouldBeShown) dismiss()
                 }
             }
@@ -54,26 +44,15 @@ class ProgressDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val title: Int = arguments?.getInt("title") ?: R.string.progress_dialog_title_placeholder
 
-        _binding = ProgressDialogBinding.inflate(layoutInflater)
-        progressBar = binding.progressHorizontal
-        textPercentages = binding.progressPercentages
-        textValue = binding.progressValue
-        currentProcess = binding.currentProcess
+        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.progress_dialog, null, false)
+        binding.lifecycleOwner = this
+        binding.model = model
 
         return activity?.let {
             MaterialAlertDialogBuilder(it).setTitle(title).setView(binding.root)
                 .setCancelable(false).create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
-
-    private fun getPercentageString() = requireContext().getString(
-        R.string.progress_dialog_percentage,
-        (progressBar.progress.toFloat() / progressBar.max) * 100
-    )
-
-    private fun getValueString() = requireContext().getString(
-        R.string.progress_dialog_value, progressBar.progress, progressBar.max
-    )
 
     companion object {
         fun newInstance(title: Int): ProgressDialogFragment {
