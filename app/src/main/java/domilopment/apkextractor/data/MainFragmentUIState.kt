a67e0ed9385d549e25abb1e7e5cd26cdb42b4b6b
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2023-11-25 15:27:20 +0100

    remove UpdateTrigger from AppListUIState

--- app/src/main/java/domilopment/apkextractor/data/MainFragmentUIState.kt
+++ app/src/main/java/domilopment/apkextractor/data/MainFragmentUIState.kt
@@ -6,5 +6,4 @@ data class MainFragmentUIState(
     var isRefreshing: Boolean = true,
     var appList: List<ApplicationModel> = listOf(),
     var actionMode: Boolean = false,
-    var updateTrigger: UpdateTrigger = UpdateTrigger(false) // State Flow does not recognize if property of element in AppList changes, so we trigger an update manually
 )
