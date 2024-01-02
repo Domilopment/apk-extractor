84488dd6e04b820de25941bbe7c399fa8613678f
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-01-02 15:32:46 +0100

    remove old App Save Name Dialog

--- app/src/main/java/domilopment/apkextractor/ui/apkNamePreferenceDialog/ApkNamePreference.kt
+++
@@ -1,8 +0,0 @@
-package domilopment.apkextractor.ui.apkNamePreferenceDialog
-
-import android.content.Context
-import android.util.AttributeSet
-import androidx.preference.MultiSelectListPreference
-
-class ApkNamePreference(context: Context, attrs: AttributeSet) :
-    MultiSelectListPreference(context, attrs)
