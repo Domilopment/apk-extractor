175a173848c324d5b9440624ad4f195f80c4f8f1
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-01-04 15:33:29 +0100

    Update ApkSortOptions/Menu

--- app/src/main/java/domilopment/apkextractor/ui/dialogs/ApkSortMenu.kt
+++ app/src/main/java/domilopment/apkextractor/ui/dialogs/ApkSortMenu.kt
@@ -1,9 +1,10 @@
 package domilopment.apkextractor.ui.dialogs
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.SheetState
@@ -11,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import domilopment.apkextractor.R
 import domilopment.apkextractor.utils.settings.ApkSortOptions
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,65 +25,19 @@ fun ApkSortMenu(
     sort: (ApkSortOptions) -> Unit
 ) {
     if (expanded) ModalBottomSheet(
-        onDismissRequest = onDismissRequest,
-        modifier = modifier,
-        sheetState = sheetState
+        onDismissRequest = onDismissRequest, modifier = modifier, sheetState = sheetState
     ) {
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_name_asc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_FILE_NAME_ASC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_FILE_NAME_ASC, onClick = null
-                )
-            })
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_name_desc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_FILE_NAME_DESC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_FILE_NAME_DESC, onClick = null
-                )
-            })
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_mod_date_desc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_LAST_MODIFIED_DESC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_LAST_MODIFIED_DESC,
-                    onClick = null
-                )
-            })
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_mod_date_asc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_LAST_MODIFIED_ASC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_LAST_MODIFIED_ASC, onClick = null
-                )
-            })
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_size_desc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_FILE_SIZE_DESC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_FILE_SIZE_DESC, onClick = null
-                )
-            })
-        DropdownMenuItem(text = { Text(text = stringResource(id = R.string.menu_sort_apk_file_size_asc)) },
-            onClick = {
-                sort(ApkSortOptions.SORT_BY_FILE_SIZE_ASC)
-            },
-            leadingIcon = {
-                RadioButton(
-                    selected = sortOrder == ApkSortOptions.SORT_BY_FILE_SIZE_ASC, onClick = null
-                )
-            })
+        ApkSortOptions.entries.forEach {
+            ListItem(headlineContent = { Text(text = stringResource(id = it.displayNameRes)) },
+                modifier = Modifier.clickable {
+                    sort(it)
+                },
+                leadingContent = {
+                    RadioButton(
+                        selected = sortOrder == it, onClick = null
+                    )
+                })
+        }
         Spacer(modifier = Modifier.navigationBarsPadding())
     }
 }
--- app/src/main/java/domilopment/apkextractor/utils/settings/ApkSortOptions.kt
+++ app/src/main/java/domilopment/apkextractor/utils/settings/ApkSortOptions.kt
@@ -1,31 +1,40 @@
 package domilopment.apkextractor.utils.settings
 
+import androidx.annotation.StringRes
+import domilopment.apkextractor.R
 import domilopment.apkextractor.data.PackageArchiveModel
 
 enum class ApkSortOptions {
     SORT_BY_FILE_NAME_ASC {
         override val comparator = compareBy(PackageArchiveModel::fileName)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_name_asc
     },
     SORT_BY_FILE_NAME_DESC {
         override val comparator = compareByDescending(PackageArchiveModel::fileName)
-    },
-    SORT_BY_FILE_SIZE_ASC {
-        override val comparator = compareBy(PackageArchiveModel::fileSize)
-    },
-    SORT_BY_FILE_SIZE_DESC {
-        override val comparator = compareByDescending(PackageArchiveModel::fileSize)
-    },
-    SORT_BY_LAST_MODIFIED_ASC {
-        override val comparator = compareBy(PackageArchiveModel::fileLastModified)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_name_desc
     },
     SORT_BY_LAST_MODIFIED_DESC {
         override val comparator = compareByDescending(PackageArchiveModel::fileLastModified)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_mod_date_desc
+    },
+    SORT_BY_LAST_MODIFIED_ASC {
+        override val comparator = compareBy(PackageArchiveModel::fileLastModified)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_mod_date_asc
+    },
+    SORT_BY_FILE_SIZE_DESC {
+        override val comparator = compareByDescending(PackageArchiveModel::fileSize)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_size_desc
+    },
+    SORT_BY_FILE_SIZE_ASC {
+        override val comparator = compareBy(PackageArchiveModel::fileSize)
+        override val displayNameRes: Int = R.string.menu_sort_apk_file_size_asc
     };
 
     abstract val comparator: Comparator<PackageArchiveModel>
+    @get:StringRes abstract val displayNameRes: Int
 
     companion object {
-        private val apkSortOptionsArray = ApkSortOptions.values().associateBy { it.name }
+        private val apkSortOptionsArray = entries.associateBy { it.name }
             .withDefault { SORT_BY_LAST_MODIFIED_DESC }
 
         operator fun get(sortMode: String?): ApkSortOptions {
