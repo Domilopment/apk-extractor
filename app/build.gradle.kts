882314ec9e62a9fa40c2be0c4d05e9c108df3574
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-04-29 20:03:43 +0200

    Update Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "domilopment.apkextractor"
         minSdk = 26
         targetSdk = 34
-        versionCode = 86
-        versionName = "2.1.6"
+        versionCode = 88
+        versionName = "2.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
