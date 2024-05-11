bb43b24b093753c41ac053f9828bbc6f452ee114
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-05-11 21:29:47 +0200

    Update InstallerUseCaseModule.kt

--- app/src/main/java/domilopment/apkextractor/dependencyInjection/useCase/InstallerUseCaseModule.kt
+++ app/src/main/java/domilopment/apkextractor/dependencyInjection/useCase/InstallerUseCaseModule.kt
@@ -1,24 +1,32 @@
 package domilopment.apkextractor.dependencyInjection.useCase
 
-import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
-import domilopment.apkextractor.domain.usecase.appList.AddAppUseCase
+import domilopment.apkextractor.dependencyInjection.installation.InstallationRepository
 import domilopment.apkextractor.domain.usecase.installer.InstallUseCase
 import domilopment.apkextractor.domain.usecase.installer.InstallUseCaseImpl
+import domilopment.apkextractor.domain.usecase.installer.UninstallUseCase
+import domilopment.apkextractor.domain.usecase.installer.UninstallUseCaseImpl
 
 @InstallIn(ActivityRetainedComponent::class)
 @Module
 object InstallerUseCaseModule {
     @Provides
     @Reusable
-    fun getAddAppUseCase(
-        @ApplicationContext context: Context, addAppUseCase: AddAppUseCase
+    fun getInstallUseCase(
+        installationsRepository: InstallationRepository
     ): InstallUseCase {
-        return InstallUseCaseImpl(context, addAppUseCase)
+        return InstallUseCaseImpl(installationsRepository)
+    }
+
+    @Provides
+    @Reusable
+    fun getUninstallUseCase(
+        installationsRepository: InstallationRepository
+    ): UninstallUseCase {
+        return UninstallUseCaseImpl(installationsRepository)
     }
 }
