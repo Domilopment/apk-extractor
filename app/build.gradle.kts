3d59fb425e9ff8e661431ab62a06017ac574e6b6
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-06-25 06:10:11 +0200

    Add room ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
