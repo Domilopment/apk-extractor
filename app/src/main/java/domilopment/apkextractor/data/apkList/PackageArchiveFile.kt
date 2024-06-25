3e479dbbff32658fa2dbb99942ac02439c5621cc
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-06-25 06:12:55 +0200

    rename packageArchiveModels to PackageArchiveFiles

---
+++ app/src/main/java/domilopment/apkextractor/data/apkList/PackageArchiveFile.kt
@@ -0,0 +1,13 @@
+package domilopment.apkextractor.data.apkList
+
+import android.net.Uri
+import java.io.File
+
+interface PackageArchiveFile {
+    val fileUri: Uri
+    val fileName: String
+    val fileType: String
+    val fileLastModified: Long
+    val fileSize: Long
+    fun packageArchiveInfo(): File?
+}
