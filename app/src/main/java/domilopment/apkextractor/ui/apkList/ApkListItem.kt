08067529bef3f89854da1fe81906bff30093e2b1
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-07-23 23:41:20 +0200

    Fix: Text gets cut of on higher font sizes in ListItems

--- app/src/main/java/domilopment/apkextractor/ui/apkList/ApkListItem.kt
+++ app/src/main/java/domilopment/apkextractor/ui/apkList/ApkListItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ImageBitmap
@@ -41,37 +41,11 @@ fun ApkListItem(
     apkVersionInfo: AnnotatedString?,
     onClick: () -> Unit
 ) {
-    Box {
-        ListItem(headlineContent = {
-            if (appPackageName != null) Text(
-                text = appPackageName,
-                fontSize = 12.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        },
-            modifier = Modifier
-                .height(96.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .clickable(onClick = onClick)
-                .padding(0.dp, 8.dp),
-            overlineContent = {
-                Text(
-                    text = appName ?: apkFileName,
-                    fontSize = 16.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            },
-            supportingContent = {
-                if (apkVersionInfo != null) Text(
-                    text = apkVersionInfo,
-                    fontSize = 12.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-            },
-            leadingContent = { ApkListItemAvatar(appIcon = appIcon) })
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .clickable(onClick = onClick),
+    ) {
         Text(
             text = apkFileName,
             modifier = Modifier.padding(16.dp, 0.dp),
@@ -80,21 +54,49 @@ fun ApkListItem(
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
+        ListItem(headlineContent = {
+            if (appPackageName != null) Text(
+                text = appPackageName,
+                fontSize = 12.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }, overlineContent = {
+            Text(
+                text = appName ?: apkFileName,
+                fontSize = 16.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }, supportingContent = {
+            if (apkVersionInfo != null) Text(
+                text = apkVersionInfo,
+                fontSize = 12.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }, leadingContent = { ApkListItemAvatar(appIcon = appIcon) })
     }
 }
 
 @Composable
 private fun ApkListItemAvatar(appIcon: ImageBitmap?) {
     val context = LocalContext.current
-    Image(
-        bitmap = appIcon ?: ResourcesCompat.getDrawable(
+    val icon = remember(appIcon) {
+        appIcon ?: ResourcesCompat.getDrawable(
             context.resources, android.R.drawable.sym_def_app_icon, context.theme
-        )!!.toBitmap().asImageBitmap(),
-        contentDescription = null,
-        modifier = Modifier
-            .padding(0.dp, 6.dp)
-            .size(50.dp)
-    )
+        )!!.toBitmap().asImageBitmap()
+    }
+    Box {
+        Image(
+            bitmap = icon,
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .align(Alignment.Center)
+        )
+    }
+
 }
 
 @Preview
--- app/src/main/java/domilopment/apkextractor/ui/appList/AppListItem.kt
+++ app/src/main/java/domilopment/apkextractor/ui/appList/AppListItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -81,7 +80,6 @@ fun AppListItem(
             }
         },
         modifier = Modifier
-            .height(64.dp)
             .clip(RoundedCornerShape(8.dp))
             .combinedClickable(onClick = onClick, onLongClick = onLongClick),
         supportingContent = {
