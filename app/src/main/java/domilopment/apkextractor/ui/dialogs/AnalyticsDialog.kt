73438248a216c1f05175ab5f18126b7e140f73be
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-07-29 01:03:27 +0200

    Finish localize consent dialog

--- app/src/main/java/domilopment/apkextractor/ui/dialogs/AnalyticsDialog.kt
+++ app/src/main/java/domilopment/apkextractor/ui/dialogs/AnalyticsDialog.kt
@@ -3,12 +3,9 @@ package domilopment.apkextractor.ui.dialogs
 import android.content.Context
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
@@ -21,6 +18,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,14 +28,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import domilopment.apkextractor.R
 import domilopment.apkextractor.utils.Constants
+import domilopment.apkextractor.utils.Utils
 
 @Composable
 fun AnalyticsDialog(
     onConfirmButton: (Boolean, Boolean, Boolean) -> Unit, context: Context = LocalContext.current
 ) {
+    val scrollState = rememberScrollState()
+    val endReached by remember {
+        derivedStateOf {
+            scrollState.value == scrollState.maxValue
+        }
+    }
+
     var analytics by remember {
         mutableStateOf(true)
     }
@@ -49,40 +57,25 @@ fun AnalyticsDialog(
     }
 
     AlertDialog(onDismissRequest = {}, confirmButton = {
-        TextButton(onClick = { onConfirmButton(analytics, crashlytics, performance) }) {
-            Text(text = stringResource(id = R.string.data_collection_confirm))
+        TextButton(
+            onClick = { onConfirmButton(analytics, crashlytics, performance) }, enabled = endReached
+        ) {
+            Text(text = stringResource(id = R.string.consent_dialog_confirm))
         }
     }, dismissButton = {
-        TextButton(onClick = { onConfirmButton(false, false, false) }) {
-            Text(text = stringResource(id = R.string.data_collection_dismiss))
+        TextButton(onClick = { onConfirmButton(false, false, false) }, enabled = endReached) {
+            Text(text = stringResource(id = R.string.consent_dialog_dismiss))
         }
     }, title = {
         Text(text = stringResource(id = R.string.data_collection_header))
     }, text = {
-        Column(modifier = Modifier.verticalScroll(state = rememberScrollState())) {
-            ClickableText(text = AnnotatedString("Data is collected anonymously (unique IDs per Installation on each device) and only used for provided use cases.")) {
-
-            }
-            Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                TextButton(
-                    onClick = {
-                        CustomTabsIntent.Builder().build().launchUrl(
-                            context, Uri.parse(Constants.TERMS_URL)
-                        )
-                    }, border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
-                ) {
-                    Text(text = "Terms & Conditions")
-                }
-                TextButton(
-                    onClick = {
-                        CustomTabsIntent.Builder().build().launchUrl(
-                            context, Uri.parse(Constants.PRIVACY_POLICY_URL)
-                        )
-                    }, border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
-                ) {
-                    Text(text = stringResource(id = R.string.privacy_policy_title))
-                }
-            }
+        Column(modifier = Modifier.verticalScroll(state = scrollState)) {
+            Text(
+                text = AnnotatedString(
+                    text = stringResource(id = R.string.consent_dialog_header),
+                    SpanStyle(fontWeight = FontWeight.Bold)
+                )
+            )
             Column(
                 modifier = Modifier.background(
                     color = MaterialTheme.colorScheme.surfaceVariant,
@@ -122,6 +115,28 @@ fun AnalyticsDialog(
                     },
                     colors = ListItemDefaults.colors(containerColor = Color.Transparent)
                 )
+            }
+            Text(
+                text = AnnotatedString(text = stringResource(id = R.string.consent_dialog_consent_notice))
+            )
+            val text = Utils.getAnnotatedUrlString(
+                stringResource(
+                    id = R.string.consent_dialog_footer,
+                    stringResource(id = R.string.privacy_policy_title),
+                    stringResource(id = R.string.terms_title)
+                ), Pair(
+                    stringResource(id = R.string.privacy_policy_title), Constants.PRIVACY_POLICY_URL
+                ), Pair(stringResource(id = R.string.terms_title), Constants.TERMS_URL)
+            )
+            ClickableText(
+                text = text
+            ) { offset ->
+                text.getStringAnnotations(offset, offset).find { it.tag.startsWith("URL") }
+                    ?.let { stringAnnotation ->
+                        CustomTabsIntent.Builder().build().launchUrl(
+                            context, Uri.parse(stringAnnotation.item)
+                        )
+                    }
             }
         }
     })
