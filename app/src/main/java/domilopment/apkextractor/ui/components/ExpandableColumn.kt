ed6d48bcea764619238f533a8d84a73eac40c469
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-08-02 19:02:57 +0200

    Fixing ExpandableColumn display bugs

--- app/src/main/java/domilopment/apkextractor/ui/components/ExpandableColumn.kt
+++ app/src/main/java/domilopment/apkextractor/ui/components/ExpandableColumn.kt
@@ -30,7 +30,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -62,17 +65,28 @@ fun ExpandableColumn(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically) {
             Row(
+                modifier = Modifier.weight(1f),
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = title, fontWeight = FontWeight.Bold)
+                Text(
+                    text = title,
+                    modifier = Modifier.weight(1f, fill = false),
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
                 if (items.size > 1) Text(
-                    text = hatMultipleItemsNotificationTitle, modifier = Modifier
+                    text = hatMultipleItemsNotificationTitle,
+                    modifier = Modifier
                         .background(
-                            MaterialTheme.colorScheme.secondaryContainer,
+                            MaterialTheme.colorScheme.inversePrimary,
                             shape = RoundedCornerShape(16.dp)
                         )
-                        .padding(8.dp), color = MaterialTheme.colorScheme.onSecondaryContainer
+                        .padding(horizontal = 8.dp),
+                    color = MaterialTheme.colorScheme.primary,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
                 )
             }
             Row(
@@ -116,13 +130,15 @@ fun ExpandableColumn(
     }
 }
 
+@PreviewFontScale
+@PreviewScreenSizes
 @Preview
 @Composable
 fun ExpandableColumnPreview() {
     MaterialTheme {
         Surface(modifier = Modifier.padding(8.dp)) {
             ExpandableColumn(
-                title = "title",
+                title = "Expandable column",
                 hatMultipleItemsNotificationTitle = "has more",
                 items = arrayOf("Item 1", "Item 2", "another one")
             )
