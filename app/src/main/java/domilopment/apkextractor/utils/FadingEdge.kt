74dc960c7e0141faa3bab5a8801f3e7f2116aedd
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2024-08-16 23:40:41 +0200

    Simplify FadingEdge

--- app/src/main/java/domilopment/apkextractor/utils/FadingEdge.kt
+++ app/src/main/java/domilopment/apkextractor/utils/FadingEdge.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
@@ -16,76 +15,64 @@ import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlin.math.abs
+import domilopment.apkextractor.utils.FadingEdge.BOTTOM
+import domilopment.apkextractor.utils.FadingEdge.BOTTOM_LEFT_CORNER
+import domilopment.apkextractor.utils.FadingEdge.BOTTOM_RIGHT_CORNER
+import domilopment.apkextractor.utils.FadingEdge.END
+import domilopment.apkextractor.utils.FadingEdge.START
+import domilopment.apkextractor.utils.FadingEdge.TOP
+import domilopment.apkextractor.utils.FadingEdge.TOP_LEFT_CORNER
+import domilopment.apkextractor.utils.FadingEdge.TOP_RIGHT_CORNER
 import kotlin.math.sqrt
 
-enum class EdgeOffsetValues(val value: Float) {
-    ZERO(0f), INFINITE(Float.POSITIVE_INFINITY)
-}
-
-class EdgeOffset(val x: EdgeOffsetValues, val y: EdgeOffsetValues) {
-    fun toOffset(): Offset = Offset(x.value, y.value)
-
-    companion object {
-        val Zero = EdgeOffset(EdgeOffsetValues.ZERO, EdgeOffsetValues.ZERO)
-        val Infinite = EdgeOffset(EdgeOffsetValues.INFINITE, EdgeOffsetValues.INFINITE)
-    }
+enum class FadingEdge {
+    TOP, BOTTOM, START, END, TOP_RIGHT_CORNER, TOP_LEFT_CORNER, BOTTOM_RIGHT_CORNER, BOTTOM_LEFT_CORNER
 }
 
 /**
  * A fading edge drawn by a linear gradient from start to end
- * @param start the begin of the fading edge from it is drawn drawn
- * @param end the end of the fading edge to it is drawn
+ * @param edge the anchor point for the edge on canvas
  * @param visible set if the content should be fully visible or not
  * @param size the size of the fading edge
  */
-fun Modifier.fadingEdge(start: EdgeOffset, end: EdgeOffset, visible: Boolean, size: Dp) = this
+fun Modifier.fadingEdge(edge: FadingEdge, visible: Boolean, size: Dp) = this
     .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
     .composed {
         require(size > 0.dp) { "Invalid fade width: Width must be greater than 0" }
-        
+
         val fade by animateDpAsState(
             targetValue = if (visible) 0.dp else size,
             animationSpec = spring(stiffness = Spring.StiffnessMediumLow),
             label = "FadingEdge"
         )
 
-        val width = remember(start, end) {
-            val abs = abs(end.x.value - start.x.value)
-            when {
-                abs.isNaN() -> 0f
-                else -> abs
-            }
-        }
-
-        val height = remember(start, end) {
-            val abs = abs(end.y.value - start.y.value)
-            when {
-                abs.isNaN() -> 0f
-                else -> abs
-            }
-        }
-
         drawWithContent {
             drawContent()
 
-            val fraction = when {
-                width == Float.POSITIVE_INFINITY && height == 0f -> fade.toPx() / this.size.width
-                width == 0f && height == Float.POSITIVE_INFINITY -> fade.toPx() / this.size.height
-                width == Float.POSITIVE_INFINITY && height == Float.POSITIVE_INFINITY -> fade.toPx() / sqrt(
+            val (start, end) = when (edge) {
+                TOP -> Offset.Zero to Offset(0f, this.size.height)
+                BOTTOM -> Offset(0f, this.size.height) to Offset.Zero
+                START -> Offset.Zero to Offset(this.size.width, 0f)
+                END -> Offset(this.size.width, 0f) to Offset.Zero
+                TOP_RIGHT_CORNER -> Offset(this.size.width, 0f) to Offset(0f, this.size.height)
+                TOP_LEFT_CORNER -> Offset.Zero to Offset(this.size.width, this.size.height)
+                BOTTOM_RIGHT_CORNER -> Offset(this.size.width, this.size.height) to Offset.Zero
+                BOTTOM_LEFT_CORNER -> Offset(0f, this.size.height) to Offset(
+                    this.size.width, 0f
+                )
+            }
+
+            val fraction = when (edge) {
+                START, END -> fade.toPx() / this.size.width
+                TOP, BOTTOM -> fade.toPx() / this.size.height
+                TOP_RIGHT_CORNER, TOP_LEFT_CORNER, BOTTOM_RIGHT_CORNER, BOTTOM_LEFT_CORNER -> fade.toPx() / sqrt(
                     this.size.width * this.size.width + this.size.height * this.size.height
                 )
-
-                width == 0f && height == 0f -> 0f
-                else -> error("FadingEdge fraction is not in range: width=$width, height=$height")
             }
 
             drawRect(
                 brush = Brush.linearGradient(
-                    0f to Color.Transparent,
-                    fraction to Color.Black,
-                    start = start.toOffset(),
-                    end = end.toOffset()
+                    0f to Color.Transparent, fraction to Color.Black, start = start, end = end
                 ), size = this.size, blendMode = BlendMode.DstIn
             )
         }
@@ -96,45 +83,29 @@ fun Modifier.fadingEdge(start: EdgeOffset, end: EdgeOffset, visible: Boolean, si
  * @param visible set if the content should be fully visible or not
  * @param size the size of the fading edge
  */
-fun Modifier.fadingTop(visible: Boolean, size: Dp = 32.dp) = fadingEdge(
-    start = EdgeOffset.Zero,
-    end = EdgeOffset(EdgeOffsetValues.ZERO, EdgeOffsetValues.INFINITE),
-    visible = visible,
-    size = size
-)
+fun Modifier.fadingTop(visible: Boolean, size: Dp = 32.dp) =
+    fadingEdge(edge = TOP, visible = visible, size = size)
 
 /**
  * Draws a fading edge at the bottom of the content
  * @param visible set if the content should be fully visible or not
  * @param size the size of the fading edge
  */
-fun Modifier.fadingBottom(visible: Boolean, size: Dp = 32.dp) = fadingEdge(
-    start = EdgeOffset(EdgeOffsetValues.ZERO, EdgeOffsetValues.INFINITE),
-    end = EdgeOffset.Zero,
-    visible = visible,
-    size = size
-)
+fun Modifier.fadingBottom(visible: Boolean, size: Dp = 32.dp) =
+    fadingEdge(edge = BOTTOM, visible = visible, size = size)
 
 /**
  * Draws a fading edge at the start of the content
  * @param visible set if the content should be fully visible or not
  * @param size the size of the fading edge
  */
-fun Modifier.fadingStart(visible: Boolean, size: Dp = 32.dp) = fadingEdge(
-    start = EdgeOffset.Zero,
-    end = EdgeOffset(EdgeOffsetValues.INFINITE, EdgeOffsetValues.ZERO),
-    visible = visible,
-    size = size
-)
+fun Modifier.fadingStart(visible: Boolean, size: Dp = 32.dp) =
+    fadingEdge(edge = START, visible = visible, size = size)
 
 /**
  * Draws a fading edge at the end of the content
  * @param visible set if the content should be fully visible or not
  * @param size the size of the fading edge
  */
-fun Modifier.fadingEnd(visible: Boolean, size: Dp = 32.dp) = fadingEdge(
-    start = EdgeOffset(EdgeOffsetValues.INFINITE, EdgeOffsetValues.ZERO),
-    end = EdgeOffset.Zero,
-    visible = visible,
-    size = size
-)
+fun Modifier.fadingEnd(visible: Boolean, size: Dp = 32.dp) =
+    fadingEdge(edge = END, visible = visible, size = size)
