92eaebc516018cb4df0adc007ca567ba7c9dcd30
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-03-10 01:02:53 +0100

    Only start AutoBackupService from TileService when Notifications are enabled

--- app/src/main/java/domilopment/apkextractor/autoBackup/AutoBackupTileService.kt
+++ app/src/main/java/domilopment/apkextractor/autoBackup/AutoBackupTileService.kt
@@ -1,6 +1,11 @@
 package domilopment.apkextractor.autoBackup
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.widget.Toast
@@ -38,6 +43,35 @@ class AutoBackupTileService : TileService() {
     // Called when the user taps on your tile in an active or inactive state.
     override fun onClick() {
         super.onClick()
+
+        if (!areNotificationsEnabled(this)) {
+            // Notifications not enabled, open settings to allow the user to enable notifications
+            Toast.makeText(
+                applicationContext,
+                R.string.auto_backup_tile_service_notifications_disabled,
+                Toast.LENGTH_LONG
+            ).show()
+
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                val pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                startActivityAndCollapse(pendingIntent)
+            } else {
+                startActivityAndCollapse(intent)
+            }
+
+            return
+        }
+
         if (AutoBackupService.isRunning) {
             updateAutoBackupPreference(false)
             Intent(this, AutoBackupService::class.java).apply {
@@ -77,5 +111,16 @@ class AutoBackupTileService : TileService() {
         runBlocking {
             settings.setAutoBackupService(value)
         }
+    }
+
+    /**
+     * Check if notifications are enabled for this app
+     * @param context Context for access to notification manager
+     * @return True if notifications are enabled
+     */
+    private fun areNotificationsEnabled(context: Context): Boolean {
+        // Check if notifications are enabled using NotificationManager
+        val manager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        return manager.areNotificationsEnabled()
     }
 }
