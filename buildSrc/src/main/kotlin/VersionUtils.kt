aa20f455ea900ae9656361534ae9c03c4fc82d32
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-03-26 20:19:17 +0100

    Update VersionUtils.kt

--- buildSrc/src/main/kotlin/VersionUtils.kt
+++ buildSrc/src/main/kotlin/VersionUtils.kt
@@ -3,5 +3,8 @@ import java.util.Date
 import java.util.Locale
 
 object VersionUtils {
+    /**
+     * Get the current build date in the format of yyyy.MM.dd
+     */
     fun buildDate(): String = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(Date())
 }
