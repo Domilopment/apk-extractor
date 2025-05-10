eaf21fabb6b76c3b4a8c52fc83255853598c3507
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-05-10 17:48:19 +0200

    Fix; reported Firebase Crashlytics error

--- app/src/main/java/domilopment/apkextractor/autoBackup/AutoBackupTileService.kt
+++ app/src/main/java/domilopment/apkextractor/autoBackup/AutoBackupTileService.kt
@@ -54,6 +54,7 @@ class AutoBackupTileService : TileService() {
 
             val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
                 putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
