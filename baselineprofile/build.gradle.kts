6eab1c483f8cbcd36d785e6d3d7c4414f4abf32d
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-07-21 22:05:01 +0200

    Update baselineprofile target sdk to 36

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
