cf38dc4aff823ccd6d871ad260a7d3b4c5ec9394
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-11-06 21:26:31 +0100

    Add back animations to ApkExtractorBottomBar

--- app/src/main/java/domilopment/apkextractor/ui/bottomBar/ApkExtractorBottomBar.kt
+++ app/src/main/java/domilopment/apkextractor/ui/bottomBar/ApkExtractorBottomBar.kt
@@ -1,6 +1,8 @@
 package domilopment.apkextractor.ui.bottomBar
 
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.AppBarRow
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,32 +10,53 @@ import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FlexibleBottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import domilopment.apkextractor.data.AppBarState
 import domilopment.apkextractor.data.UiState
 import domilopment.apkextractor.ui.DeviceTypeUtils
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun ApkExtractorBottomBar(
     appBarState: AppBarState, uiState: UiState, modifier: Modifier = Modifier
 ) {
-    when {
-        uiState is UiState.ActionMode && DeviceTypeUtils.isPhoneBars && appBarState.actionModeActions.isNotEmpty() -> ActionModeBar(
-            modifier = modifier, items = appBarState.actionModeActions
-        )
-
-        else -> Unit
+    val isPhoneBars = DeviceTypeUtils.isPhoneBars
+    val isVisible by remember(uiState, appBarState, isPhoneBars) {
+        derivedStateOf {
+            uiState is UiState.ActionMode && isPhoneBars && appBarState.actionModeActions.isNotEmpty()
+        }
     }
+
+    val animatedProgress by animateFloatAsState(
+        targetValue = if (isVisible) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 300, delayMillis = 200, easing = FastOutSlowInEasing
+        ),
+        label = "BottomBarHeightAnimation",
+    )
+
+    if (animatedProgress > 0f) ActionModeBar(
+        modifier = modifier.alpha(animatedProgress),
+        items = appBarState.actionModeActions,
+        expandedHeightDp = BottomAppBarDefaults.FlexibleBottomAppBarHeight * animatedProgress
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-private fun ActionModeBar(modifier: Modifier = Modifier, items: List<BottomBarItem>) {
+private fun ActionModeBar(
+    modifier: Modifier = Modifier, items: List<BottomBarItem>, expandedHeightDp: Dp
+) {
     FlexibleBottomAppBar(
         modifier = modifier,
-        horizontalArrangement = BottomAppBarDefaults.FlexibleFixedHorizontalArrangement
+        horizontalArrangement = BottomAppBarDefaults.FlexibleFixedHorizontalArrangement,
+        expandedHeight = expandedHeightDp
     ) {
         val context = LocalContext.current
         AppBarRow {
