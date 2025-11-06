b211cd7efe4097bd1b59786cc32f6044c13861e7
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-11-06 19:35:21 +0100

    Update DeviceTypeUtils.kt

--- app/src/main/java/domilopment/apkextractor/ui/DeviceTypeUtils.kt
+++ app/src/main/java/domilopment/apkextractor/ui/DeviceTypeUtils.kt
@@ -3,8 +3,7 @@ package domilopment.apkextractor.ui
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
-import androidx.window.core.layout.WindowHeightSizeClass
-import androidx.window.core.layout.WindowWidthSizeClass
+import androidx.compose.ui.unit.dp
 
 object DeviceTypeUtils {
     enum class DeviceType {
@@ -13,12 +12,10 @@ object DeviceTypeUtils {
 
     fun getDeviceBarType(adaptiveInfo: WindowAdaptiveInfo): DeviceType {
         return with(adaptiveInfo) {
-            if (windowPosture.isTabletop || windowSizeClass.windowHeightSizeClass == WindowHeightSizeClass.COMPACT) {
+            if (windowSizeClass.minWidthDp.dp == 0.dp || windowPosture.isTabletop || windowSizeClass.minHeightDp.dp == 0.dp) {
                 DeviceType.PHONE
-            } else if (windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.EXPANDED || windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.MEDIUM) {
-                DeviceType.TABLET
             } else {
-                DeviceType.PHONE
+                DeviceType.TABLET
             }
         }
     }
