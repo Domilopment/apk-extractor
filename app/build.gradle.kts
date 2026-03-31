d183f249d605860730b5f8c99f94c11656a46831
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2026-03-31 17:09:24 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material3.navigationSuite)
-    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3.navigation3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.datastore.preferences)
