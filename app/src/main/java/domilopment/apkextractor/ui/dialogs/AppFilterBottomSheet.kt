eedea99a5d82aeb4bde7bb2065e888110467bca4
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2026-08-10 16:53:58 +0200

    Update dependencies

--- app/src/main/java/domilopment/apkextractor/ui/dialogs/AppFilterBottomSheet.kt
+++ app/src/main/java/domilopment/apkextractor/ui/dialogs/AppFilterBottomSheet.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
@@ -44,6 +43,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.ToggleButton
 import androidx.compose.material3.ToggleButtonDefaults
+import androidx.compose.material3.ToggleButtonShapes
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -54,6 +54,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -328,9 +331,12 @@ private fun AppFilterSort(
                         onCheckedChange = {
                             sortApps(item.ordinal)
                         },
-                        modifier = Modifier.fillMaxWidth(),
-                        shapes = ToggleButtonDefaults.shapes(
+                        modifier = Modifier
+                            .semantics { role = Role.RadioButton }
+                            .fillMaxWidth(),
+                        shapes = ToggleButtonShapes(
                             shape = shape,
+                            pressedShape = ToggleButtonDefaults.pressedShape,
                             checkedShape = ButtonGroupDefaults.connectedButtonCheckedShape,
                         ),
                     ) {
